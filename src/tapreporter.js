ded43e15056562dbf566eb4f10d0cd6487876c6e
Koji NAKAMURA <kozy4324@gmail.com>
2012-02-14 17:19:02 +0900

    implement TAPReporter.log

--- src/tapreporter.js
+++ src/tapreporter.js
@@ -34,36 +34,44 @@
         TAPReporter.prototype.reportSpecStarting = function(spec) {};
 
         TAPReporter.prototype.reportSpecResults = function(spec) {
-          var buf, item, line, msg, msgs, _i, _j, _k, _len, _len2, _len3, _ref, _results;
+          var buf, item, line, msg, msgs, _i, _j, _k, _l, _len, _len2, _len3, _len4, _ref, _ref2, _results;
           this.count++;
           buf = [];
+          _ref = spec.results().getItems();
+          for (_i = 0, _len = _ref.length; _i < _len; _i++) {
+            item = _ref[_i];
+            if (item.type === 'log') buf.push("# " + item.values[0]);
+          }
           if (spec.results().passed()) {
             buf.push("ok " + this.count + " - " + (spec.getFullName()));
           } else {
             buf.push("not ok " + this.count + " - " + (spec.getFullName()));
             msgs = [];
-            _ref = spec.results().getItems();
-            for (_i = 0, _len = _ref.length; _i < _len; _i++) {
-              item = _ref[_i];
+            _ref2 = spec.results().getItems();
+            for (_j = 0, _len2 = _ref2.length; _j < _len2; _j++) {
+              item = _ref2[_j];
               if (item.type === 'expect' && !item.passed()) {
                 msgs = msgs.concat(item.message.split(/\r\n|\r|\n/));
               }
             }
-            for (_j = 0, _len2 = msgs.length; _j < _len2; _j++) {
-              msg = msgs[_j];
+            for (_k = 0, _len3 = msgs.length; _k < _len3; _k++) {
+              msg = msgs[_k];
               buf.push("# " + msg);
             }
           }
           _results = [];
-          for (_k = 0, _len3 = buf.length; _k < _len3; _k++) {
-            line = buf[_k];
+          for (_l = 0, _len4 = buf.length; _l < _len4; _l++) {
+            line = buf[_l];
             this.results_.push(line);
             _results.push(typeof this.print === "function" ? this.print(line) : void 0);
           }
           return _results;
         };
 
-        TAPReporter.prototype.log = function(str) {};
+        TAPReporter.prototype.log = function(str) {
+          this.results_.push("# " + str);
+          return typeof this.print === "function" ? this.print("# " + str) : void 0;
+        };
 
         return TAPReporter;
 
